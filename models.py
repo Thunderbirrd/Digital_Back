55112221afb83bc5d312bde1b4a1f5421787c8fc
Artem <artemis.gorb.14@gmail.com>
2020-10-16 19:12:48 +0300

    auth

---
+++ models.py
@@ -0,0 +1,71 @@
+from database import db
+from flask import session
+
+
+class Model:
+    errors = []
+
+    def validate(self):
+        self.errors = []
+        return len(self.errors) == 0
+
+    def save(self):
+        if self.validate():
+            if self.id is None:
+                db.session.add(self)
+            db.session.commit()
+            return True
+        else:
+            return False
+
+
+class User(db.Model, Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(), unique=True)
+    password = db.Column(db.String())
+    name = db.Column(db.String())
+    surname = db.Column(db.String())
+    lastname = db.Column(db.String())
+
+    def __init__(self, login="", password="", name="", surname="", lastname=""):
+        self.login = login
+        self.password = password
+        self.name = name
+        self.surname = surname
+        self.lastname = lastname
+        self.save()
+
+    def set_login(self, new_login):
+        self.login = new_login
+        self.save()
+
+    def set_password(self, new_pass):
+        self.password = new_pass
+        self.save()
+
+    @staticmethod
+    def get_user_by_login(login):
+        return db.session.query(User).filter(User.login == login).first()
+
+    @staticmethod
+    def get_current_user():
+        id = session.get("auth")
+        return db.session.query(User).filter(User.id == id).first()
+
+    @staticmethod
+    def is_authorised():
+        return User.get_current_user() is not None
+
+    @staticmethod
+    def get_login_by_id(i):
+        user = db.session.query(User).filter(User.id == i).first()
+        return user.login
+
+    @staticmethod
+    def check_login_is_unique(login):
+        return db.session.query(User.login).filter(User.login == login).first() is None
+
+    @staticmethod
+    def auth(login, password):
+        return db.session.query(User).filter(User.login == login).filter(User.password == password).first()
---
+++ routes.py
@@ -0,0 +1,45 @@
+from app import app
+from flask import request, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import User
+import json
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    if request.form:
+        login = request.form.get("login")
+        password = request.form.get("password")
+        name = request.form.get("name")
+        surname = request.form.get("surname")
+        lastname = request.form.get("lastname")
+        if User.check_login_is_unique(login):
+            user = User(login, generate_password_hash(password), name, surname, lastname)
+            user.save()
+            return f"New user with login {user.login}"
+        else:
+            return "Такой логин уже занят"
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form:
+        login = request.form.get("login")
+        password = request.form.get("password")
+        user = User.get_user_by_login(login)
+        if user and check_password_hash(user.password, password):
+            session["auth"] = user.id
+            d = {
+                "login": user.login,
+                "name": user.name,
+                "surname": user.surname,
+                "lastname": user.lastname
+            }
+            return json.dumps(d)
+        else:
+            return "Wrong login and/or password"
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello, world"
