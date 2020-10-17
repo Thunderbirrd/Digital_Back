3867ab5150cd5981d5c1e2f631d7407c9bc9f5e2
Vadim <vadimmm120@yandex.ru>
2020-10-17 14:25:58 +0300

    board create

--- routes.py
+++ routes.py
@@ -87,7 +87,7 @@ def index():
 
 @app.route('/create_board', methods=['POST'])
 def create_table():
-    if request.form:
+    if request:
         admin = request.form.get("admin")
         name = request.form.get("name")
         table = TaskTable(name, admin)
@@ -95,18 +95,17 @@ def create_table():
 
         short_desc = request.form.get("short_desc")
         leader = request.form.get("leader")
-        parent = 0
         executor = request.form.get("executor")
         taskboard_id = table.id
         intensity = request.form.get("difficulty")
         desc = request.form.get("desc")
         deadline = request.form.get("deadline")
         is_single_task = False
-        task = Task(short_desc, leader, parent, executor, taskboard_id, intensity, desc, deadline, is_single_task)
+        task = Task(leader, taskboard_id, deadline, short_desc, intensity, None, executor, desc, is_single_task)
         task.save()
 
-        for tag in request.form.get("tags"):
-            TaskTag(task.id, tag)
+        # for tag in request.form.get("tags"):
+        #     TaskTag(task.id, tag)
 
-        return json.dumps({"taskid":task.id, "tableid":table.id})
+        return json.dumps({"taskid": task.id, "tableid": table.id})
 
