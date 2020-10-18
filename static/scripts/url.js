c44aae7a6ac8338e67f1f77225b4130f287996f6
Egor <belyaev.eda@gmail.com>
2020-10-18 05:03:49 +0300

    not tested fetch

--- static/scripts/url.js
+++ static/scripts/url.js
@@ -6,6 +6,149 @@ const register = async (login, password, name, surname, lastname) => {
         },
         body: JSON.stringify({login, password, name, surname, lastname})
     });
+    return await responce.json();
+}
 
+
+const get_user_id_by_login = async (login) => {
+    const responce = await fetch("/get_user_id_by_login", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({login})
+    });
+    return await responce.json();
+}
+
+
+const login = async (login, password) => {
+    const responce = await fetch("/login", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({login, password})
+    });
+    return await responce.json();
+}
+
+
+const create_child = async (parent, leader, executor, deadline, short_desc, desc, difficulty, tags) => {
+    const responce = await fetch("/createChild", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({parent, leader, executor, deadline, short_desc, desc, difficulty, tags})
+    });
+    return await responce.json();
+}
+
+
+const get_task_by_id = async (id) => {
+    const responce = await fetch("/get_task_by_id", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({id})
+    });
+    return await responce.json();
+}
+
+
+const get_tag_by_id = async (id) => {
+    const responce = await fetch("/get_tag_by_id", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({id})
+    });
+    return await responce.json();
+}
+
+
+const get_task_list = async (id) => {
+    const responce = await fetch("/getListTask", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({id})
+    });
+    return await responce.json();
+}
+
+
+const create_tag = async (name, tasktable_id, user_id) => {
+    const responce = await fetch("/create_tag", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({name, tasktable_id, user_id})
+    });
+    return await responce.json();
+}
+
+
+const create_board = async (admin, name, short_desc, leader, executor, difficulty, desc, deadline) => {
+    const responce = await fetch("/create_board", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({admin, name, short_desc, leader, executor, difficulty, desc, deadline})
+    });
+    return await responce.json();
+}
+
+
+const change_task_status = async (id) => {
+    const responce = await fetch("/change_task_status", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({id})
+    });
+    return await responce.json();
+}
+
+
+const create_single_task = async (short_desc, desc, leader, executor, difficulty, deadline, tags) => {
+    const responce = await fetch("/create_single_task", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({short_desc, desc, leader, executor, difficulty, deadline, tags})
+    });
+    return await responce.json();
+}
+
+
+const get_tree = async (id) => {
+    const responce = await fetch("/get_tree", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({id})
+    });
+    return await responce.json();
+}
+
+
+const delete_task = async (id) => {
+    const responce = await fetch("/delete_task", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({id})
+    });
     return await responce.json();
 }
